7b0cfaab3eaf7c13aa29a36330bc6d1f3074a634
Adam Chambers <adammchambers@gmail.com>
2019-04-29 22:12:44 -0400

    🚧  Example of workload fetching for single user

--- index.js
+++ index.js
@@ -18,6 +18,22 @@ request(
     // parse auth keys
     const authData = JSON.parse(body)
 
-    console.log(authData)
+    request(
+      {
+        method: 'GET',
+        url: `https://api.teamgantt.com/v1/workload/users?ids=${
+          authData.user_id
+        }&group_by=week&start_date=2019-04-29&end_date=2019-05-25`,
+        headers: {
+          'Content-Type': 'application/json',
+          'TG-Authorization': `Bearer ${config.teamgantt.bearer}`,
+          'TG-Api-Key': authData.api_key,
+          'TG-User-Token': authData.user_token
+        }
+      },
+      (error, response, body) => {
+        console.log(JSON.parse(body)[0])
+      }
+    )
   }
 )
