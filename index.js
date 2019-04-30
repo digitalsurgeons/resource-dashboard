c6fd7711ccf4c73d12ac5c72b3b417f4b115f29e
Adam Chambers <adammchambers@gmail.com>
2019-04-29 22:31:53 -0400

    🚧  Beginnings of looping through departments and fetching workloads

--- index.js
+++ index.js
@@ -18,22 +18,35 @@ request(
     // parse auth keys
     const authData = JSON.parse(body)
 
-    request(
-      {
-        method: 'GET',
-        url: `https://api.teamgantt.com/v1/workload/users?ids=${
-          authData.user_id
-        }&group_by=week&start_date=2019-04-29&end_date=2019-05-25`,
-        headers: {
-          'Content-Type': 'application/json',
-          'TG-Authorization': `Bearer ${config.teamgantt.bearer}`,
-          'TG-Api-Key': authData.api_key,
-          'TG-User-Token': authData.user_token
+    Object.keys(config.teamgantt.departments).forEach(department => {
+      const users = config.teamgantt.departments[department]
+        .map(user => {
+          return user.id
+        })
+        .join(',')
+
+      request(
+        {
+          method: 'GET',
+          url: `https://api.teamgantt.com/v1/workload/users?ids=${users}&group_by=week&start_date=2019-04-29&end_date=2019-05-25`,
+          headers: {
+            'Content-Type': 'application/json',
+            'TG-Authorization': `Bearer ${config.teamgantt.bearer}`,
+            'TG-Api-Key': authData.api_key,
+            'TG-User-Token': authData.user_token
+          }
+        },
+        (error, response, body) => {
+          console.log('')
+          console.log(department)
+          JSON.parse(body).forEach(user => {
+            console.log(`User ${user.type_id}`)
+            user.data.forEach(workload => {
+              console.log(workload.date, workload.hours_total)
+            })
+          })
         }
-      },
-      (error, response, body) => {
-        console.log(JSON.parse(body)[0])
-      }
-    )
+      )
+    })
   }
 )
